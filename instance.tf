fd91c386b01f7fdc9ad983899bd9230cafb36233
Piyush Somani <81923351+Piyush-Somani@users.noreply.github.com>
2021-09-23 00:11:24 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,26 +1,26 @@
-provider "google" {
-  project = "poised-vial-319809"
-  region  = "us-central1"
-  zone    = "us-central1-a"
-}
+# provider "google" {
+#   project = "poised-vial-319809"
+#   region  = "us-central1"
+#   zone    = "us-central1-a"
+# }
 
-resource "google_compute_instance" "resource_name" {
-  name         = "tst"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
+# resource "google_compute_instance" "resource_name" {
+#   name         = "tst"
+#   machine_type = "e2-micro"
+#   zone         = "us-central1-a"
 
-  tags = ["foo", "bar"]
+#   tags = ["foo", "bar"]
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-  network_interface {
-    network = "default"
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-9"
+#     }
+#   }
+#   network_interface {
+#     network = "default"
 
-    access_config {
-      // Ephemeral IP
-    }
-  }
-}
+#     access_config {
+#       // Ephemeral IP
+#     }
+#   }
+# }
